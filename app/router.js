0781641fa1d778bbdd45608867de96079508a0b5
Khedir Qassim <79780374+KhedirQ@users.noreply.github.com>
2023-12-11 15:47:22 -0600

    Update router.js

--- app/router.js
+++ app/router.js
@@ -1,9 +1,15 @@
+// app/router.js
+
 import EmberRouter from '@ember/routing/router';
-import config from 'ycc-scheduling/config/environment';
+import config from 'appointment-booking-frontend/config/environment';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('appointments');
+  // ... other routes if needed
+});
+// app/templates/appointments.hbs
